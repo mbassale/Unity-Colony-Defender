9a3c15c874757492e69ede96f7c9d249e8c81242
Marco Bassaletti <bassaletti@gmail.com>
2019-07-30 19:17:02 -0400

    Created a SelfDestroyer script.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,8 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(deathFX, transform.position, Quaternion.identity);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
---
+++ Assets/SelfDestroyer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroyer : MonoBehaviour
+{
+    [SerializeField] float ttl = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, ttl);
+    }
+}
