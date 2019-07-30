c1ac598383159e170318ecaba12d3c975c102388
Marco Bassaletti <bassaletti@gmail.com>
2019-07-30 00:56:12 -0400

    Basic death sequence.

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float loadLevelDelay = 1f;
+    [SerializeField] GameObject deathFX;
+    
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
@@ -13,5 +17,12 @@ public class CollisionHandler : MonoBehaviour
     private void StartDeathSequence()
     {
         SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke(nameof(ReloadLevel), loadLevelDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
