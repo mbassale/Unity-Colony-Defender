4e81ed8a8bbfeafd592202c2df31b59996a062a6
Marco Bassaletti <bassaletti@gmail.com>
2019-07-30 19:08:16 -0400

    Added enemy explosions.

---
+++ Assets/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Add Non Trigger Box Collider
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Instantiate(deathFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
