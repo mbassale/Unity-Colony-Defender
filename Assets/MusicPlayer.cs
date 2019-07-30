1e5ec7018f9d52ef3f2673d47626da42c5a95ed0
Marco Bassaletti <bassaletti@gmail.com>
2019-07-29 23:59:18 -0400

    Separated scene loading from MusicPlayer script.

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -5,23 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    const float SPLASH_DELAY = 3f;
-
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (SceneManager.GetActiveScene().buildIndex == 0) {
-            Invoke(nameof(LoadFirstScene), SPLASH_DELAY);
-        }
-    }
-
-    void LoadFirstScene()
-    {
-        SceneManager.LoadScene("Level1");
     }
 }
---
+++ Assets/SceneLoader.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    const float SPLASH_DELAY = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Invoke(nameof(LoadFirstScene), SPLASH_DELAY);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
