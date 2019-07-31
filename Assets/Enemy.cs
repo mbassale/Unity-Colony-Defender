4f2293472cf6d4deadfbcf95f2ef9662855e5795
Marco Bassaletti <bassaletti@gmail.com>
2019-07-31 02:35:40 -0400

    Enemy store score per hit.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
 
     ScoreBoard scoreBoard;
 
@@ -20,7 +21,7 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
-        scoreBoard.ScoreHit();
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
