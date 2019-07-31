59b3406bec21cfc80772d5fede8d3e81aa764156
Marco Bassaletti <bassaletti@gmail.com>
2019-07-31 02:30:28 -0400

    Increase score when hitting enemies.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,9 +7,12 @@ public class Enemy : MonoBehaviour
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
 
+    ScoreBoard scoreBoard;
+
     // Start is called before the first frame update
     void Start()
     {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
         // Add Non Trigger Box Collider
         Collider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
@@ -17,6 +20,7 @@ public class Enemy : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        scoreBoard.ScoreHit();
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
         Destroy(gameObject);
